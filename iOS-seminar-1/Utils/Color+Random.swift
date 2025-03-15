33d440774e43d24f7cc93d58b99e596d8d260076
lroofer <lroofer@yandex-team.ru>
2025-03-15 13:34:56 +0300

    fixes

--- iOS-seminar-1/Utils/Color+Random.swift
+++ iOS-seminar-1/Utils/Color+Random.swift
@@ -8,48 +8,48 @@
 import UIKit
 
 extension UIColor {
-  static var random: UIColor {
-    randomHEX.color
-  }
-
-  static var randomHEX: String {
-    let letters = "0123456789ABCDEF"
-    var color = "#"
-
-    for _ in 0..<6 {
-      color += String(letters.randomElement()!)
+    static var random: UIColor {
+        randomHEX.color
     }
-
-    return color
-  }
-
-  static func getUniqueColors(count: Int) -> [UIColor] {
-    var set = Set<UIColor>()
-
-    while set.count < count {
-      set.insert(.random)
+    
+    static var randomHEX: String {
+        let letters = "0123456789ABCDEF"
+        var color = "#"
+        
+        for _ in 0..<6 {
+            color += String(letters.randomElement()!)
+        }
+        
+        return color
     }
-
-    return set.shuffled()
-  }
+    
+    static func getUniqueColors(count: Int) -> [UIColor] {
+        var set = Set<UIColor>()
+        
+        while set.count < count {
+            set.insert(.random)
+        }
+        
+        return set.shuffled()
+    }
 }
 
 private extension String {
-  var color: UIColor {
-    let hexSanitized = self
-      .trimmingCharacters(in: .whitespacesAndNewlines)
-      .replacingOccurrences(of: "#", with: "")
-
-    var rgb: UInt64 = 0
-
-    guard Scanner(string: hexSanitized).scanHexInt64(&rgb) else {
-      return .black
+    var color: UIColor {
+        let hexSanitized = self
+            .trimmingCharacters(in: .whitespacesAndNewlines)
+            .replacingOccurrences(of: "#", with: "")
+        
+        var rgb: UInt64 = 0
+        
+        guard Scanner(string: hexSanitized).scanHexInt64(&rgb) else {
+            return .black
+        }
+        
+        let r = CGFloat((rgb & 0xFF0000) >> 16) / 255.0
+        let g = CGFloat((rgb & 0x00FF00) >> 8) / 255.0
+        let b = CGFloat(rgb & 0x0000FF) / 255.0
+        
+        return UIColor(red: r, green: g, blue: b, alpha: 1.0)
     }
-
-    let r = CGFloat((rgb & 0xFF0000) >> 16) / 255.0
-    let g = CGFloat((rgb & 0x00FF00) >> 8) / 255.0
-    let b = CGFloat(rgb & 0x0000FF) / 255.0
-
-    return UIColor(red: r, green: g, blue: b, alpha: 1.0)
-  }
 }
--- iOS-seminar-1/ViewController.swift
+++ iOS-seminar-1/ViewController.swift
@@ -7,50 +7,51 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+final class ViewController: UIViewController {
 
-  @IBOutlet var views: [UIView]!
-  @IBOutlet weak var button: UIButton!
+    // MARK: - Variables
 
-  private var updatingInterfaceTask: TaskGroup<Void>?
+    @IBOutlet var views: [UIView]!
+    @IBOutlet weak var button: UIButton!
 
-  override func viewDidLoad() {
-    super.viewDidLoad()
+    // MARK: - Internal functions
+    override func viewDidLoad() {
+        super.viewDidLoad()
 
-    updateViews()
-  }
-
-  @IBAction func buttonWasPressed(_ sender: UIButton) {
-    updateViews()
-  }
-
-  @MainActor private func updateViews() {
-    guard let button else {
-      return
+        updateViews()
     }
-    button.isEnabled = false
-    UIView.animate(withDuration: .animationDuration) { [weak self] in
-      guard let views = self?.views else { return }
-      UIColor.getUniqueColors(count: views.count).enumerated().forEach { (index, color) in
-        guard index < views.count else {
-          return
+
+    @IBAction func buttonWasPressed(_ sender: UIButton) {
+        updateViews()
+    }
+
+    // MARK: - Private functions
+
+    @MainActor private func updateViews() {
+        guard let button else {
+            return
         }
-        views[index].backgroundColor = color
-        views[index].layer.cornerRadius = .random(
-          in: CGFloat.minCornerRadius...CGFloat.maxCornerRadius
-        )
-      }
-    } completion: { _ in
-      button.isEnabled = true
+        button.isEnabled = false
+        UIView.animate(withDuration: Constants.animationDuration) { [weak self] in
+            guard let views = self?.views else { return }
+            UIColor.getUniqueColors(count: views.count).enumerated().forEach { (index, color) in
+                guard index < views.count else {
+                    return
+                }
+                views[index].backgroundColor = color
+                views[index].layer.cornerRadius = .random(
+                    in: Constants.minCornerRadius...Constants.maxCornerRadius
+                )
+            }
+        } completion: { _ in
+            button.isEnabled = true
+        }
     }
-  }
 }
 
-private extension CGFloat {
-  static let minCornerRadius: Self = 0
-  static let maxCornerRadius: Self = 25
-}
+private enum Constants {
+    static let minCornerRadius: CGFloat = 0
+    static let maxCornerRadius: CGFloat = 25
 
-private extension TimeInterval {
-  static let animationDuration: Self = 3
+    static let animationDuration: TimeInterval = 3
 }
